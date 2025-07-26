ef7613f27fe53a989481b6b9a1505eddeac46fa8
jarrenjames13 <72928144+jarrenjames13@users.noreply.github.com>
2025-07-27 03:45:47 +0800

    Update lua-retriever.lua

--- lua-retriever.lua
+++ lua-retriever.lua
@@ -7,9 +7,27 @@ end
 
 -- Hook HttpGet
 local old_httpget = game.HttpGet
-game.HttpGet = function(self, url, ...)
-    local response = old_httpget(self, url, ...)
-    print("URL:", url)
-    print("Loaded Script:\n", response)
-    return response
+if old_httpget then
+    -- For older Roblox versions that might use dot notation
+    game.HttpGet = function(self, url, ...)
+        local response = old_httpget(self, url, ...)
+        print("URL:", url)
+        print("Loaded Script:\n", response)
+        return response
+    end
+else
+    -- For newer Roblox versions that use the proper method call with colon
+    local mt = getmetatable(game)
+    local old_index = mt.__index
+    mt.__index = newcclosure(function(self, key)
+        if key == "HttpGet" then
+            return function(self, url, ...)
+                local response = game:HttpGet(url, ...)
+                print("URL:", url)
+                print("Loaded Script:\n", response)
+                return response
+            end
+        end
+        return old_index(self, key)
+    end)
 end
